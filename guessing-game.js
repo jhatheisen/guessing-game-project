44cb9a1c5aa5e1be98028756425c66b3e41940d6
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-13 14:37:52 -0700

    Finished phase 2

--- guessing-game.js
+++ guessing-game.js
@@ -5,7 +5,51 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let secretNumber = undefined
+let secretNumber;
+
+function askRange(){
+  rl.question('Enter a max number: ', handleMaxNum)
+}
+
+function handleMaxNum(answer) {
+  let maxNum = Number(answer)
+  if(isNaN(maxNum)){
+    console.log("Not A Number!")
+    askRange()
+  }
+
+  secretNumber = maxNum;
+  rl.question("Enter a min number: ", handleMinNum)
+}
+
+function handleMinNum(answer) {
+  let minNum = Number(answer)
+  if(isNaN(minNum)){
+    console.log("Not A Number!")
+    rl.question("Enter a min number: ", handleMinNum)
+  }
+
+  secretNumber = randomInRange(minNum, secretNumber)
+  askGuess()
+}
+
+function randomInRange(min, max){
+  let randomNum = Math.floor(Math.random() * (max - min) + min)
+  return randomNum
+}
+
+function askGuess() {
+  rl.question("Enter a guess:", (answer) => {
+    let correct = checkGuess(answer);
+
+    if (correct) {
+      console.log("You win!");
+      rl.close();
+    } else {
+      askGuess();
+    }
+  });
+}
 
 function checkGuess(num) {
   num = Number(num);
@@ -23,59 +67,5 @@ function checkGuess(num) {
     return true;
   }
 }
-// checkGuess(1);
-// checkGuess(100);
-// checkGuess(10);
 
-function askGuess() {
-  rl.question("Enter a guess:", (answer) => {
-    let correct = checkGuess(answer);
-
-    if (correct) {
-      console.log("You win!");
-      rl.close();
-    } else {
-      askGuess();
-    }
-  });
-}
-
-function randomInRange(min, max){
-  let randomNum = Math.floor(Math.random() * (max - min) + min)
-  return randomNum
-}
-
-// console.log(randomInRange(15, 20)); // 16
-// console.log(randomInRange(15, 20)); // 17
-// console.log(randomInRange(20, 20)); // 20
-
-const handleMaxNum = (answer) => {
-  let maxNum = Number(answer)
-  if(isNaN(maxNum)){
-    console.log("Not A Number!")
-    askRange()
-  }
-
-  rl.question("Enter a min number: ", handleMinNum())
-}
-
-const handleMinNum = (answer, maxNum) => {
-  let minNum = Number(answer)
-  if(isNaN(minNum)){
-    console.log("Not A Number!")
-    rl.question("Enter a min number: ", handleMinNum)
-  }
-  console.log("minNum is ", minNum, "maxNum is ", maxNum)
-  secretNumber = randomInRange(minNum, maxNum)
-  // askGuess()
-}
-
-function askRange(){
-  rl.question('Enter a max number: ', handleMaxNum)
-}
-
-
-
-// askGuess();
 askRange()
-console.log(secretNumber)
